31f516b45ea928e212e9db8eeeec77d138d6040c
Denis Dymerlii <dum.denya15@gmail.com>
2024-03-17 12:08:51 +0200

    add multipartparser

--- cabinet/views.py
+++ cabinet/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.parsers import MultiPartParser, JSONParser
 from rest_framework.permissions import IsAuthenticated
 
 from cabinet.models import Post
@@ -9,6 +10,7 @@ class PostView(generics.CreateAPIView, generics.RetrieveUpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, JSONParser]
 
     def perform_update(self, serializer):
         serializer.save(status="New")

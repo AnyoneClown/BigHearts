0bf7665636a762d8afe356515bd1701b25c3abf5
Denis Dymerlii <dum.denya15@gmail.com>
2024-03-27 20:14:02 +0200

    fix model field

--- cabinet/models.py
+++ cabinet/models.py
@@ -32,7 +32,7 @@ class Post(models.Model):
         regex=r"^\+?1?\d{9,15}$",
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
     )
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True, null=True)
 
     telegram = models.CharField(max_length=100, blank=True, null=True)
     location = models.CharField(max_length=255, blank=True, null=True)
